f9f12f056e1530857333f45d87e29ef61d33798e
www0002 <www0002@gmail.com>
2015-06-19 07:32:27 +0300

    jmtr

--- js/app.js
+++ js/app.js
@@ -274,7 +274,7 @@ app.controller('homeCtrl', function($scope, mainFactory, cartFactory) {
 			//,text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' + ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
 		});
 	};
-	for (var i=0; i<2; i++) {
+	for (var i=0; i < 21; i++) {
 		$scope.addSlide();
 	}
 	
